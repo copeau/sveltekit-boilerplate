a9ce6dc9012a0a2178425745cf03316cab216ebf
francois godineau <francois.godineau@gmail.com>
2024-10-12 10:15:30 +0200

    add auth.js with google auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+  import { signIn, signOut } from "@auth/sveltekit/client"
+  import { page } from "$app/stores"
+</script>
+
+{#if $page.data.session}
+  <nav>
+    <img
+      src={$page.data.session?.user?.image ?? "https://i.pravatar.cc/300"}
+      alt="User Avatar"
+    />
+  <button on:click={signOut}>sign out</button>
+  </nav>
+{:else}
+  <div>
+    <img src="/favicon.ico" alt="Company Logo" />
+    <button on:click={signIn}>sign in</button>
+  </div>
+{/if}
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
